4eb9600a839cc26de47e850b33335bcb8eeb14af
Victor Ramos Mello <772792+m0nad@users.noreply.github.com>
2020-01-01 15:48:25 +0100

    fix action cable for 127.0.0.1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,5 +40,5 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_cable.url = 'wss://localhost:28080'
-  config.action_cable.allowed_request_origins = [ 'localhost', /localhost/  ]
+  config.action_cable.allowed_request_origins = [ 'http://127.0.0.1:3000', '127.0.0.1', 'localhost', /localhost/  ]
 end
